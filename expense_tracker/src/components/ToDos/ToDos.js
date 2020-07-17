98c758461385703337346c0a7838e617a5a6b1a0
Devdutt Jangir <devdutt.jangir@3pillarglobal.com>
2020-07-17 15:51:07 +0530

    add and remove todo code integrated

--- expense_tracker/src/components/ToDos/ToDos.js
+++ expense_tracker/src/components/ToDos/ToDos.js
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import React, { Component } from "react";
 
 export default class ToDos extends Component {
@@ -7,8 +7,9 @@ export default class ToDos extends Component {
     this.state = { todos: [] };
   }
   filterCollections = (data) => {
-    const todo = data.map((item) => {
+    const todo = data.map((item, index) => {
       return {
+        id: index,
         url: item.url,
         description: item.description,
         avatar_url: item.owner.avatar_url,
@@ -17,6 +18,7 @@ export default class ToDos extends Component {
     });
     this.setState({ todos: todo });
   };
+
   getCollections = () => {
     const URL = "https://api.github.com/gists/public";
     fetch(URL)
@@ -24,23 +26,45 @@ export default class ToDos extends Component {
       .then((data) => this.filterCollections(data));
   };
 
+  addTodo = () => {
+    // form call
+    // description, url, username
+    const username = Math.random().toString(36).substring(7);
+    const description = "New Todo " + username;
+    const url =
+      "https://gist.github.com/rajivmehtaflex/c49bf48821b6a745b0301363f3311324";
+    const avatar_url = "https://image.flaticon.com/icons/svg/145/145867.svg";
+
+    const { todos } = this.state;
+
+    const newTodo = {
+      id: todos.length,
+      url: url,
+      description: description,
+      avatar_url: avatar_url,
+      username: username,
+    };
+
+    todos.splice(0, 0, newTodo);
+    this.setState({
+      todos: todos,
+    });
+  };
+
+  removeTodo = (id) => {
+    const { todos } = this.state;
+    const index = id.id;
+    const updatedTodos = todos.filter((item) => item.id !== index);
+    this.setState({
+      todos: updatedTodos,
+    });
+  };
+
   renderCollections = () => {
     const { todos } = this.state;
     return todos.map((item) => {
-      const { url, description, avatar_url, username } = item;
+      const { url, description, avatar_url, username, id } = item;
       return (
-        // <Col className="col-4 d-flex flex-grow-1 p-2">
-        //   <Card style={{ width: "18rem" }}>
-        //     <Card.Img variant="top" src={avatar_url} height={"200px"} />
-
-        //     <Card.Body>
-        //       <Card.Title>{username}</Card.Title>
-        //       <Card.Subtitle>{url}</Card.Subtitle>
-        //       <Card.Text>{description}</Card.Text>
-        //     </Card.Body>
-
-        //   </Card>
-        // </Col>
         <Col className="col-4 d-flex flex-grow-1">
           <Container className="m-1 border border-secondary rounded">
             <Row>
@@ -50,7 +74,20 @@ export default class ToDos extends Component {
               <Col className="p-0">
                 <Container>
                   <Row>
-                    <Col className="font-weight-bold">{username}</Col>
+                    <Col className="font-weight-bold">
+                      <div>
+                        {username}
+                        <button
+                          type="button"
+                          className="close"
+                          aria-label="Close"
+                          id={id}
+                          onClick={() => this.removeTodo({ id })}
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    </Col>
                   </Row>
                   <Row>
                     <Col className="text-wrap font-weight-light">
@@ -78,8 +115,11 @@ export default class ToDos extends Component {
     return (
       <Container>
         <Row>
-          <Col>
+          <Col className="col-10">
             <h3>ToDos</h3>
+          </Col>
+          <Col>
+            <Button onClick={() => this.addTodo()}>Add Todo</Button>
           </Col>
         </Row>
         <Row>{this.renderCollections()}</Row>
